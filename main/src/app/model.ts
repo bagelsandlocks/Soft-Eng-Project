8758a5c56de8db059549ac196de08208c5319d72
Biwen <92959026+BiwenDong@users.noreply.github.com>
2025-11-19 13:19:10 -0500

    Add models for Shopper, Item, Store, and Receipt

---
+++ main/src/app/model.ts
@@ -0,0 +1,121 @@
+// quick and dirty. Put everything into one class. Should separate
+
+export class Shopper {
+    username: string
+    password: string
+    shoppingList: Array<ShoppingList>
+    reciept: Array<Receipt>
+    storeChain: Array<storeChain>
+
+    constructor () {
+        this.username = ""
+        this.password = ""
+        this.shoppingList = []
+        this.reciept = []
+        this.storeChain = []
+    }
+
+}
+
+export class ShoppingList {
+    SL_ID: string
+    items: Array<Item>
+
+    constructor() {
+        this.SL_ID = ""
+        this.items = []
+    }
+
+}
+
+export class storeChain {
+    name: string
+    stores: Array<Store>
+    url: string
+
+    constructor() {
+        this.name = ""
+        this.stores = []
+        this.url = ""
+    }
+
+}
+
+export class Item {
+    name: string
+    id: string
+    price: number
+    quantity: number
+
+    constructor( name: string, id: string, price: number, quantity: number) {
+        this.name = name
+        this.id = id
+        this.price = price
+        this.quantity = quantity
+    }
+
+}
+
+export class Store {
+    name: string
+    items: Array<Item>
+    url: string
+    reciepts: Array<Receipt>
+
+    constructor() {
+        this.name = ""
+        this.items = []
+        this.url = ""
+        this.reciepts = []
+    }
+    
+}
+
+export class Administrator {
+    username: string
+    password: string
+
+    constructor() {
+        this.username = ""
+        this.password = ""
+    }
+}
+
+export class Receipt {  
+    receiptID: string
+    shopper: string         // shopper username
+    storeName: string
+    storeChainName: string
+    status: string           // "draft" | "submitted"
+    items: Array<Item> = []
+
+    constructor() {
+
+        this.receiptID = ""
+        this.shopper = ""
+        this.storeName = ""
+        this.storeChainName = ""
+        this.status = ""
+        this.items = []
+    }
+
+    
+}
+ 
+
+
+
+
+
+
+    // setUsername(username: string) {
+    //     this.username = username
+    // }
+
+    // setPassword(password: string) {
+    //     this.password = password
+    // }
+
+    // comparePassword(input: string) : boolean {
+    //     return this.password === input;
+    // }
